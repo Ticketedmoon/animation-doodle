89a042b920474e635c1e3d3dd68541c4157b96bf
Shane <Shane.creedon3@mail.dcu.ie>
2018-01-25 19:25:31 +0000

    Added 'Clear Canvas' Button to the drawing screen

--- code/app/src/main/java/ca326/com/activities/Sign_In_Screen.java
+++ code/app/src/main/java/ca326/com/activities/Sign_In_Screen.java
@@ -38,9 +38,7 @@ import static android.Manifest.permission.READ_CONTACTS;
  */
 public class Sign_In_Screen extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
+    // Id to identity READ_CONTACTS permission request.
     private static final int REQUEST_READ_CONTACTS = 0;
 
     /**
