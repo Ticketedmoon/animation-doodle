0d292f6fd1314003766af7f3584b93a42d6728bb
James Collins <james.collins36@mail.dcu.ie>
2018-01-29 15:19:59 +0000

    fixed password masking

--- code/app/src/main/java/ca326/com/activities/Sign_In_Screen.java
+++ code/app/src/main/java/ca326/com/activities/Sign_In_Screen.java
@@ -27,6 +27,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.text.method.PasswordTransformationMethod;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -70,6 +71,8 @@ public class Sign_In_Screen extends AppCompatActivity implements LoaderCallbacks
         populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
+        // convert passord to asterix
+        mPasswordView.setTransformationMethod(new changePasswordToAsterix());
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
@@ -80,6 +83,7 @@ public class Sign_In_Screen extends AppCompatActivity implements LoaderCallbacks
                 return false;
             }
         });
+
 
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setTextColor(getApplication().getResources().getColor(R.color.colorWhite));
@@ -375,6 +379,29 @@ public class Sign_In_Screen extends AppCompatActivity implements LoaderCallbacks
         }
 
     }
+
+    public class changePasswordToAsterix extends PasswordTransformationMethod {
+        @Override
+        public CharSequence getTransformation(CharSequence source, View view) {
+            return new GetCharSequence(source);
+        }
+
+        private class GetCharSequence implements CharSequence {
+            private CharSequence mText;
+            public GetCharSequence(CharSequence text) {
+                mText = text;
+            }
+            public char charAt(int index) {
+                return '*'; //Replace with an asterix here
+            }
+            public int length() {
+                return mText.length();
+            }
+            public CharSequence subSequence(int start, int end) {
+                return mText.subSequence(start, end);
+            }
+        }
+    };
 }
 
 
