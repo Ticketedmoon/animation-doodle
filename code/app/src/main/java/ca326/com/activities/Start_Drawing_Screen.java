12b17760b2683f62a9b6ef8ec7014714b85ff65b
Shane <shane.creedon3@mail.dcu.ie>
2018-02-22 22:02:13 +0000

    Progress
    Signed-off-by: Shane <Shane.creedon3@mail.dcu.ie>

--- code/app/src/main/java/ca326/com/activities/Start_Drawing_Screen.java
+++ code/app/src/main/java/ca326/com/activities/Start_Drawing_Screen.java
@@ -269,7 +269,7 @@ public class Start_Drawing_Screen extends AppCompatActivity implements MyRecycle
     }
 
     public static Bitmap loadBitmapFromView(View v) {
-        Bitmap b = Bitmap.createBitmap( 800, 600, Bitmap.Config.ARGB_8888);
+        Bitmap b = Bitmap.createBitmap( 64, 64, Bitmap.Config.ARGB_8888);
         Canvas c = new Canvas(b);
         v.layout(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
         v.draw(c);
