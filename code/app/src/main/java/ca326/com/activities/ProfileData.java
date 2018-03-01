cfacfa5f781b7ad5bd480031e920d1fc78839ce9
James Collins <james.collins36@mail.dcu.ie>
2018-03-01 12:32:04 +0000

    Link profile features to database

---
+++ code/app/src/main/java/ca326/com/activities/ProfileData.java
@@ -0,0 +1,61 @@
+package ca326.com.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static ca326.com.activities.Profile_Screen.textViewAbout;
+import static ca326.com.activities.Sign_In_Screen.user_id;
+
+public class ProfileData extends AppCompatActivity {
+
+    public JsonArrayRequest getData(Integer id) {
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest("http://animationdoodle2017.com/profile.php?id=" + String.valueOf(id),
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        parseData(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(ProfileData.this, "Error getting data. Make sure your internet connection is working", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        //Returning the request
+        return jsonArrayRequest;
+    }
+
+    private void parseData(JSONArray array) {
+        for (int i = 0; i < array.length(); i++) {
+
+            JSONObject json = null;
+            try {
+                //Getting json
+                json = array.getJSONObject(i);
+
+                textViewAbout.setText(json.getString("text"));
+                Log.i("textviewabout","text is " + textViewAbout);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
