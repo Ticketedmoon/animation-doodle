6fb11b3402590cee7e98e6b53fda95777140c706
James Collins <james.collins36@mail.dcu.ie>
2018-03-05 16:55:41 +0000

    ..

--- code/app/src/test/java/ca326/com/activities/LoginTest.java
+++ code/app/src/test/java/ca326/com/activities/LoginTest.java
@@ -11,10 +11,31 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static junit.framework.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LoginTest {
+
+
+    @Test
+    public void email_validation(){
+        ItemTwoFragment signIn = new ItemTwoFragment();
+        boolean result = signIn.isEmailValid("James@j.com");
+        assertEquals(true,result);
+        boolean negativeResult = signIn.isEmailValid("James@j/com");
+        assertEquals(false,negativeResult);
+    }
+
+    @Test
+    public void password_validation() {
+        ItemTwoFragment signIn = new ItemTwoFragment();
+        boolean result = signIn.isPasswordValid("Knock5");
+        assertEquals(true, result);
+        boolean negativeResult = signIn.isPasswordValid("Knock");
+        assertEquals(false, negativeResult);
+    }
+
 
     @Mock private ItemTwoFragment mock;
 
@@ -24,8 +45,9 @@ public class LoginTest {
     }
 
     @Test
-    public void test_loginButtonClick_1() {
-        doCallRealMethod().when(mock).attemptLogin("james@james.com","Wellowwellow%5");
+    public void test_login() {
+        doCallRealMethod().when(mock).attemptLogin("james@j.com","Knock5");
+        //verify(mock, times(1)).startActivity(any(Intent.class));
     }
 
 
--- code/app/src/test/java/ca326/com/activities/VideoDetailsTesting.java
+++ code/app/src/test/java/ca326/com/activities/VideoDetailsTesting.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 
-public class ExampleUnitTest {
+public class VideoDetailsTesting {
 
     @Test
     public void video_validation() throws Exception {
