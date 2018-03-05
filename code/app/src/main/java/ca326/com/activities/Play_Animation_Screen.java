9c7d1860b077b75b614c530edd423b45011ed50c
Shane <shane.creedon3@mail.dcu.ie>
2018-03-05 18:56:54 +0000

    Big improvements to Play_Animation Screen
    Signed-off-by: Shane <Shane.creedon3@mail.dcu.ie>

--- code/app/src/main/java/ca326/com/activities/Play_Animation_Screen.java
+++ code/app/src/main/java/ca326/com/activities/Play_Animation_Screen.java
@@ -1,16 +1,24 @@
 package ca326.com.activities;
 
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.Pair;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.TranslateAnimation;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.util.List;
 import java.util.Map;
@@ -22,10 +30,15 @@ public class Play_Animation_Screen extends AppCompatActivity {
     // Private fields
     private CanvasViewNonEditable cv;
     private Integer pos;
-    private View myView;
-    private View myTopView;
+    private RelativeLayout myView;
+    private RelativeLayout myTopView;
+
     private Boolean isUp;
-    private Button myButton;
+    private Boolean isPlaying = true;
+
+    private SeekBar seek;
+    private ImageButton change_canvas;
+    private ImageButton pause_play_btn;
 
     // Public Static
     public static Integer frame_rate;
@@ -45,16 +58,88 @@ public class Play_Animation_Screen extends AppCompatActivity {
         this.pos = 0;
         this.cv = (CanvasViewNonEditable) findViewById(R.id.canvas2);
 
+        // Create important textViews
+        TextView valueTV = new TextView(this);
+        valueTV.setText(Start_Drawing_Screen.ANIMATION_TITLE);
+        Typeface face = Typeface.createFromAsset(getAssets(),
+                "fonts/CaviarDreams.ttf");
+        valueTV.setTypeface(face);
+        valueTV.setTextSize(TypedValue.COMPLEX_UNIT_SP,20);
+        valueTV.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+
         myView = findViewById(R.id.my_view_bottom);
         myView.setVisibility(View.VISIBLE);
 
         myTopView = findViewById(R.id.my_view_top);
         myTopView.setVisibility(View.VISIBLE);
+        myTopView.setGravity(Gravity.CENTER);
+        myTopView.addView(valueTV);
 
-        myButton = findViewById(R.id.my_button);
+        // Image Button modifications
+        change_canvas = (ImageButton) findViewById(R.id.changeBG);
+        change_canvas.setBackgroundColor(Color.TRANSPARENT);
+        change_canvas.setImageResource(R.drawable.change_bg_click);
+
+        // Image Button modifications
+
+        pause_play_btn = (ImageButton) findViewById(R.id.playPauseButton);
+        pause_play_btn.setBackgroundColor(Color.TRANSPARENT);
+        pause_play_btn.setImageResource(R.drawable.exomedia_ic_pause_white);
+        pause_play_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (isPlaying)
+                    pause_play_btn.setImageResource(R.drawable.exomedia_ic_play_arrow_white);
+                else
+                    pause_play_btn.setImageResource(R.drawable.exomedia_ic_pause_white);
+
+                isPlaying = !isPlaying;
+            }
+        });
+
+        // SeekBar manipulations
+        seek = (SeekBar) findViewById(R.id.playAnimationSeekBar);
+
+        seek.setMax(pathways.size()-1);
+        seek.setProgress(0);
+
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //add code here
+                Log.i("Play Animation SeekBar", "SeekBar no longer touched");
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //add code here
+                Log.i("Play Animation SeekBar", "SeekBar touched");
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBark, int progress, boolean fromUser) {
+                //add code here
+                Log.i("Play Animation SeekBar", "SeekBar slider moved!");
+                Log.i("Play Animation SeekBar", "Value: " + (progress));
+
+                if (progress != seekBark.getMax()) {
+                    pos = progress;
+                    cv.newPaths = pathways.get(pos);
+                    cv.invalidate();
+                }
+            }
+        });
+
+
+        // Ending
         isUp = true;
+        cv.postDelayed(new Runnable() {
 
-        play_animation(cv);
+            @Override
+            public void run() {
+                play_animation(cv);
+            }
+        }, 1000);
     }
 
     public void play_animation(View v) {
@@ -69,20 +154,22 @@ public class Play_Animation_Screen extends AppCompatActivity {
         m_handler = new Handler();
         m_handlerTask = new Runnable() {
             public void run() {
-                System.out.println(pos.toString() + "----" + Integer.toString(pathways.size()));
-                cv.newPaths = pathways.get(pos);
-                cv.setBackground(drawables.get(pos));
-                cv.invalidate();
-                pos++;
+                Log.i("Running", "isPlaying: " + isPlaying);
+                if (isPlaying) {
+                    seek.setProgress(pos);
+                    System.out.println(pos.toString() + "----" + Integer.toString(pathways.size()));
+                    cv.newPaths = pathways.get(pos);
+                    cv.setBackground(drawables.get(pos));
+                    cv.invalidate();
+                    pos++;
+                }
+                    // Delay needs to be here for pause / play button reasons
+                    // instead of 1000 mention the delay in milliseconds
+                    m_handler.postDelayed(m_handlerTask, frame_rate);
 
-                // Delay needs to be here for pause / play button reasons
-                // instead of 1000 mention the delay in milliseconds
-                m_handler.postDelayed(m_handlerTask, frame_rate);
-
-                // Doesn't require {} body since only 1 line following the IF conditional.
-                if (pos == pathways.size())
-                    pos=0;
-
+                    // Doesn't require {} body since only 1 line following the IF conditional.
+                    if (pos == pathways.size())
+                        pos = 0;
             }
         };
         m_handlerTask.run();
@@ -106,38 +193,70 @@ public class Play_Animation_Screen extends AppCompatActivity {
 
     // Animation Methods for Sliding toolbar play button menu
     // slide the view from below itself to the current position
-    public void slideUp(View view){
+    public void slideUpBottom(View view){
         view.setVisibility(View.VISIBLE);
         TranslateAnimation animate = new TranslateAnimation(
                 0,                 // fromXDelta
                 0,                 // toXDelta
                 view.getHeight(),  // fromYDelta
                 0);                // toYDelta
-        animate.setDuration(500);
+        animate.setDuration(300);
         animate.setFillAfter(true);
         view.startAnimation(animate);
     }
 
     // slide the view from its current position to below itself
-    public void slideDown(View view){
+    public void slideDownBottom(View view){
         TranslateAnimation animate = new TranslateAnimation(
                 0,                 // fromXDelta
                 0,                 // toXDelta
                 0,                 // fromYDelta
                 view.getHeight()); // toYDelta
-        animate.setDuration(500);
+        animate.setDuration(300);
         animate.setFillAfter(true);
         view.startAnimation(animate);
     }
 
-    public void onSlideViewButtonClick(View view) {
+    public void slideUpTop(View view){
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                0,
+                -view.getHeight());             // toYDelta
+        animate.setDuration(300);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
+
+    // slide the view from its current position to below itself
+    public void slideDownTop(View view){
+        view.setVisibility(View.VISIBLE);
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                -view.getHeight(),  // fromYDelta
+                0);                 // fromYDelta
+        animate.setDuration(300);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
+
+    public void onSlideViewClick(View view) {
         if (isUp) {
-            slideDown(myView);
-            myButton.setText("Slide up");
+            slideDownBottom(myView);
+            slideUpTop(myTopView);
         } else {
-            slideUp(myView);
-            myButton.setText("Slide down");
+            slideUpBottom(myView);
+            slideDownTop(myTopView);
         }
         isUp = !isUp;
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == android.view.MotionEvent.ACTION_UP)
+            onSlideViewClick(cv);
+
+        Log.i("OnTouch", "Delaying...");
+        return true;
     }
 }
